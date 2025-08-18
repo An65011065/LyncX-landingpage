50bd97fbcc10602ad092830d42121d8eb689e53f
An Adhikari <anweshana@middlebury.edu>
2025-08-18 16:34:47 -0700

    changing redirect url

--- src/spotifyConfig.ts
+++ src/spotifyConfig.ts
@@ -1,6 +1,6 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 export const clientId = "10a750cb31ec4fdc8340f04df8438d4f";
-export const redirectUri = "http://localhost:5173/";
+export const redirectUri = "https://lyncx.ai/play";
 export const scopes = [
     "user-read-currently-playing",
     "user-read-playback-state",
