bc7a9e38e948aba2c4db46e532d450e41e825b5c
An Adhikari <anweshana@middlebury.edu>
2025-08-24 16:25:41 -0700

    media rerouting

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    publicDir: 'assets',
     assetsInclude: ['**/*.mp4', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif'],
 });
